963da6ecd73d137c98e9cbe164a6ac3c2a6fdf31
OlehBrov <o.dudnyk85@gmail.com>
2023-01-20 16:48:29 +0200

    task-01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyRef = document.querySelector("body");
+let isActive = true;
+let timerId = null;
+
+startBtn.addEventListener('click', startColors);
+stopBtn.addEventListener('click', stopColors);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+ 
+
+function startColors (){
+if(isActive === true){
+    timerId = setInterval(() => {
+        bodyRef.style.backgroundColor = getRandomHexColor();  
+        }, 1000)
+        isActive = false;
+}
+return;
+    
+}
+
+function stopColors(){
+    clearInterval(timerId)
+    isActive = true;
+
+  }
