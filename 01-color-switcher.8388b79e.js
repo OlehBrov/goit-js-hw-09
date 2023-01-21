c5af86e9da38bd3db6693e1212ec7067fd050ffb
OlehBrov <111384529+OlehBrov@users.noreply.github.com>
2023-01-21 14:44:28 +0000

    Deploying to gh-pages from @ OlehBrov/goit-js-hw-09@4df0757ef3fcb2dc6a07c8349017a580ea568fd2 🚀

--- 01-color-switcher.8388b79e.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=!0,r=null;t.addEventListener("click",(function(){!0===o&&(r=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),o=!1);return})),e.addEventListener("click",(function(){clearInterval(r),o=!0}))}();
-//# sourceMappingURL=01-color-switcher.8388b79e.js.map
--- 01-color-switcher.8900cd41.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");let o=!0,r=null;t.addEventListener("click",(function(){!0===o&&(r=setInterval((()=>{n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),o=!1);return})),e.addEventListener("click",(function(){clearInterval(r),o=!0}));
-//# sourceMappingURL=01-color-switcher.8900cd41.js.map
