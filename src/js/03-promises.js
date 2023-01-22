c9c01d9d2203c74dfd4679bfcfa4aa284c627616
OlehBrov <o.dudnyk85@gmail.com>
2023-01-22 19:16:09 +0200

    tast-03 start

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,70 @@
+import Notiflix from 'notiflix';
+
+refs = {
+  form: document.querySelector('.form'),
+  submitBtn: document.querySelector('button')
+}
+
+const userData = {};
+let promiseCounter = 1;
+let timeOutId = null;
+let intervalId = null;
+
+refs.form.addEventListener('input', storeData);
+refs.submitBtn.addEventListener('click', onSubmit)
+
+function storeData(e){
+ userData[e.target.name] = e.target.value;
+ console.log(userData.delay)
+
+}
+console.log(userData)
+console.log(userData.delay)
+
+function onSubmit(e){
+  e.preventDefault();
+  // if(userData.amount < promiseCounter) {
+  //   return;
+  // }
+
+timeOutId = setTimeout (() => {
+  delayedPromise()
+  }, userData.delay);
+
+if (userData.amount === promiseCounter){
+  clearInterval(intervalId)
+};
+
+  intervalId = setInterval(() => {
+    intervalPromise
+  }, userData.step); 
+ 
+}
+
+function delayedPromise(){
+  createPromise(promiseCounter, userData.step);
+  promiseCounter += 1;
+  clearTimeout(timeOutId)
+}
+
+function intervalPromise(){
+  createPromise(promiseCounter, userData.step);
+  promiseCounter += 1;
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`); 
   } else {
-    // Reject
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
   }
 }
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
