7cda8a1a2c4e75cf6367187fdef77771aad40d93
OlehBrov <o.dudnyk85@gmail.com>
2023-01-25 22:33:51 +0200

    task-03 complete

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,14 +7,17 @@ const form = document.querySelector('.form');
 const submitBtn = document.querySelector('button');
 
 let userData = {
+  amount: 0,
   delay: 0,
+  step: 0,
 };
-let positionCounter = 0;
+let position = 0;
 let intervalId = null;
-const delay = userData.delay;
-let delayTime = Number(userData.delay);
+let promiseDelay = 0;
 
-
+let step = 0;
+let promisePosition = 0;
+let promiseStep = 0;
 
 form.addEventListener('input', userInputSave);
 submitBtn.addEventListener('click', buildPromises)
@@ -26,34 +29,45 @@ function userInputSave(e){
 function buildPromises(e){
   e.preventDefault();
   setTimeout(() => {
+    if(Number(userData.delay) !== 0){
+      promiseDelay = Number(userData.delay)
 
-    createPromise(userData.amount, userData.step);
+    }
+    step = Number(userData.step);
+   createPromise(position, step);
     
-  }, userData.delay)
+  }, userData.delay - userData.step)
+
+
 }
 
 
 function createPromise(position, delay){
+  let increment = 0;
   return new Promise((resolve, reject) => {
-        intervalId = setInterval(() => {
-        positionCounter +=1;
-        delayTime += Number(delay);
+   
+    intervalId = setInterval(() => {
+      const shouldResolve = Math.random() > 0.3;
+      promisePosition +=1;
+      promiseStep = delay;
+      promiseDelay = promiseDelay === 0 ? delay : promiseDelay;
+      if(shouldResolve){
+          resolve (Notiflix.Notify.success(`✅ Fulfilled promise ${promisePosition} in ${promiseDelay + promiseStep*increment}ms`));
+        } else {
+          reject(Notiflix.Notify.warning(`❌ Rejected promise ${promisePosition} in ${promiseDelay + promiseStep*increment}ms`));
+        }
+        if(promisePosition === Number(userData.amount) || promisePosition === position){
+          clearInterval(intervalId)}
+          
+          increment += 1;
 
-        const shouldResolve = Math.random() > 0.3;
-          if(shouldResolve){
-            resolve (Notiflix.Notify.success(`✅ Fulfilled promise ${positionCounter} in ${delayTime}ms`));
-          } else {
-            reject(Notiflix.Notify.warning(`❌ Rejected promise ${positionCounter} in ${delayTime}ms`));
-          }
-          
-          
-          if(positionCounter === Number(userData.amount) || Number(position) === positionCounter){
-            clearInterval(intervalId)}
-        },delay)
+      },delay)
     })    
-  }
+
+}
+
   
 
-  createPromise(4, 2500).then(
-  result => {Notiflix.Notify.success(result)}, 
-  error => {Notiflix.Notify.warning(error)})
+  // createPromise(4, 3000).then(
+  // result => {(result)}, 
+  // error => {(error)})
