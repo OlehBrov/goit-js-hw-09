0da7a3a306158a6fe72094c3e6d088a21d77f9d8
OlehBrov <o.dudnyk85@gmail.com>
2023-01-27 12:25:19 +0200

    task 03 - fixes

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -16,11 +16,13 @@ let intervalId = null;
 let promiseDelay = 0;
 let delay = 0;
 
-let step = 0;
 let promisePosition = 0;
 let promiseStep = 0;
 let increment = 0;
-
+let promiseData = {
+  position,
+  delay,
+}
 form.addEventListener('input', userInputSave);
 submitBtn.addEventListener('click', buildPromises)
 
@@ -30,16 +32,32 @@ function userInputSave(e){
 
 function buildPromises(e){
   e.preventDefault();
+  
+  
   setTimeout(() => {
     // delaying first promise
-      promiseDelay = Number(userData.delay)
-    step = Number(userData.step);
-   createPromise(position, step);
+    promiseDelay = Number(userData.delay)
+    delay = Number(userData.step);
+   createPromise(position, delay).then((resolve) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${promiseData.position} in ${promiseData.delay}ms`)
+   },
+   (reject) => {
+    Notiflix.Notify.warning(`❌ Rejected promise ${promiseData.position} in ${promiseData.delay}ms`)
+   });
     
  // delaying rest of promiises
   intervalId = setInterval(() =>{
-      delay = Number(userData.step)
-        createPromise(position, delay)}, Number(userData.step));
+        createPromise(position, delay).then((resolve) => {
+          Notiflix.Notify.success(`✅ Fulfilled promise ${promiseData.position} in ${promiseData.delay}ms`)
+         },
+         (reject) => {
+          Notiflix.Notify.warning(`❌ Rejected promise ${promiseData.position} in ${promiseData.delay}ms`)
+         });}, 
+
+
+        Number(userData.step));
+
+
  }, Number(userData.delay))
 
 }
@@ -47,37 +65,23 @@ function buildPromises(e){
 
 function createPromise(position, delay){
   
-  //check, if the function isn't initiated by button to set up internal timeout
-  let internalDelay = 0;
-  if(userData.amount === 0 && userData.delay === 0 && userData.step === 0){
-    internalDelay = delay;
-    promisePosition = position-1;
-    promiseDelay = delay;
-  }     
-  //////////////////////////////////////
-  setTimeout(() => {
     return new Promise((resolve, reject) => {
       const shouldResolve = Math.random() > 0.3;
       promisePosition +=1;
       promiseStep = Number(userData.step);
 
       if(shouldResolve){
-          resolve (Notiflix.Notify.success(`✅ Fulfilled promise ${promisePosition} in ${promiseDelay + promiseStep*increment}ms`));
+          resolve (promiseData.position = promisePosition, promiseData.delay = promiseDelay + promiseStep*increment);
         } else {
-          reject(Notiflix.Notify.warning(`❌ Rejected promise ${promisePosition} in ${promiseDelay + promiseStep*increment}ms`));
+          reject (promiseData.position = promisePosition, promiseData.delay = promiseDelay + promiseStep*increment);
         }
         increment += 1;
-        if(promisePosition === Number(userData.amount) || promisePosition === position){
+        if(
+          promisePosition === Number(userData.amount) 
+          ){
           clearInterval(intervalId)}
 
 
     })    
-  }, internalDelay);
 
 }
-
-  //to run function without button >>
-
-  // createPromise(4, 3000).then(
-  // result => {(result)}, 
-  // error => {(error)})
